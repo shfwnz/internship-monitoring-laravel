d5b7b3e5c72dcc1a3deeb0fbda628451310a0d8d
shfwnz <ilhamdzaky2007@gmail.com>
2025-05-12 22:16:51 +0700

    feat(api): add api setup

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('teachers', TeacherController::class);
+Route::apiResource('students', StudentController::class);
+Route::apiResource('industries', IndustryController::class);
+Route::apiResource('internships', InternshipController::class);
