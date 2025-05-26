627fd0363b5f2ae6d6218412b2499cc2c308a414
shfwnz <ilhamdzaky2007@gmail.com>
2025-05-26 22:24:17 +0700

    fix(internship): form and view table

--- app/Filament/Admin/Resources/InternshipResource.php
+++ app/Filament/Admin/Resources/InternshipResource.php
@@ -4,40 +4,79 @@ namespace App\Filament\Admin\Resources;
 
 use App\Filament\Admin\Resources\InternshipResource\Pages;
 use App\Filament\Admin\Resources\InternshipResource\RelationManagers;
-use App\Models\Internship;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Filament\Forms\Components\Wizard;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+// Model
+use App\Models\Internship;
+use App\Models\Industry;
+use App\Models\Student;
+use App\Models\Teacher;
 
 class InternshipResource extends Resource
 {
     protected static ?string $model = Internship::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-briefcase';
 
     public static function form(Form $form): Form
     {
         return $form
+            ->columns(1)
             ->schema([
-                Forms\Components\TextInput::make('student_id')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('teacher_id')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\TextInput::make('industry_id')
-                    ->required()
-                    ->numeric(),
-                Forms\Components\DatePicker::make('start_date')
-                    ->required(),
-                Forms\Components\DatePicker::make('end_date')
-                    ->required(),
-                Forms\Components\FileUpload::make('image')
-                    ->image(),
+                Wizard::make([
+                    Wizard\Step::make('Internship Information')
+                        ->schema([
+                            Forms\Components\Select::make('student_id')
+                                ->label('Student')
+                                ->required()
+                                ->options(
+                                    Student::with('user')
+                                        ->get()
+                                        ->pluck('user.name', 'id')
+                                )
+                                ->searchable()
+                                ->preload(),
+                            Forms\Components\Select::make('teacher_id')
+                                ->label('Teacher')
+                                ->required()
+                                ->options(
+                                    Teacher::with('user')
+                                        ->get()
+                                        ->pluck('user.name', 'id')
+                                )
+                                ->searchable()
+                                ->preload(),
+                            Forms\Components\Select::make('industry_id')
+                                ->label('Industry')
+                                ->required()
+                                ->options(
+                                    Industry::all()
+                                        ->pluck('name', 'id')
+                                )
+                                ->searchable()
+                                ->preload(),
+                        ]),
+                    Wizard\Step::make('Dates and Image')
+                        ->schema([
+                            Forms\Components\FileUpload::make('image')
+                                ->label('Image')
+                                ->image()
+                                ->required()
+                                ->columnSpanFull(),
+                            Forms\Components\DatePicker::make('start_date')
+                                ->required(),
+                            Forms\Components\DatePicker::make('end_date')
+                                ->required()
+                        ])->columns(2),
+
+                ])
             ]);
     }
 
@@ -45,22 +84,26 @@ class InternshipResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('student_id')
-                    ->numeric()
+                Tables\Columns\TextColumn::make('student.user.name')
+                    ->label('Student')
+                    ->searchable()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('teacher_id')
-                    ->numeric()
+                Tables\Columns\TextColumn::make('teacher.user.name')
+                    ->label('Teacher')
+                    ->searchable()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('industry_id')
-                    ->numeric()
+                Tables\Columns\TextColumn::make('industry.name')
+                    ->label('Industry')
+                    ->searchable()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('start_date')
-                    ->date()
+                    ->date('M d, Y')
                     ->sortable(),
                 Tables\Columns\TextColumn::make('end_date')
-                    ->date()
+                    ->date('M d, Y')
                     ->sortable(),
-                Tables\Columns\ImageColumn::make('image'),
+                Tables\Columns\ImageColumn::make('image')
+                    ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
