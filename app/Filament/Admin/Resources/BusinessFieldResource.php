a593429a378eaab4493946916470488159713ebd
shfwnz <ilhamdzaky2007@gmail.com>
2025-05-27 10:47:49 +0700

    feat(business_field): add business field

---
+++ app/Filament/Admin/Resources/BusinessFieldResource.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use App\Filament\Admin\Resources\BusinessFieldResource\Pages;
+use App\Filament\Admin\Resources\BusinessFieldResource\RelationManagers;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+// Model
+use App\Models\BusinessField;
+
+class BusinessFieldResource extends Resource
+{
+    protected static ?string $model = BusinessField::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-cube-transparent';
+    protected static ?string $navigationGroup = 'Field';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255)
+                    ->unique(ignoreRecord: true)
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageBusinessFields::route('/'),
+        ];
+    }
+}
---
+++ app/Filament/Admin/Resources/BusinessFieldResource/Pages/ManageBusinessFields.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Admin\Resources\BusinessFieldResource\Pages;
+
+use App\Filament\Admin\Resources\BusinessFieldResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageBusinessFields extends ManageRecords
+{
+    protected static string $resource = BusinessFieldResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
